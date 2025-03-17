530effdff18cb2db59dda57f753eb934f9d99661
Fabio Piccolo <fabio.piccolo@u-blox.com>
2025-03-17 13:29:01 +0100

    Fix linting error on python

--- code_py/test/test_hworld.py
+++ code_py/test/test_hworld.py
@@ -3,8 +3,10 @@ sys.path.append('../')
 
 from hworld import say_hello
 
+
 def test_hworld_default():
-    assert say_hello() == 'Hello, world!'
+    assert say_hello() == 'Hello, World!'
+
 
 def test_hworld_custom():
     assert say_hello('custom') == 'Hello, custom!'
