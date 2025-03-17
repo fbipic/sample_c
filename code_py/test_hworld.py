f7efe9400b57ffe341314a90bfef673550f3b411
Fabio Piccolo <fabio.piccolo@u-blox.com>
2025-03-17 16:45:17 +0100

    Move test in main folder and remove test folder. Update file accordingly

--- code_py/test_hworld.py
+++ code_py/test_hworld.py
@@ -1,14 +1,14 @@
 """Module providing function to be tested."""
 
 
-import sys
-sys.path.append("../")
 from hworld import say_hello
 
 
 def test_hworld_default():
+    """Test with default argument."""
     assert say_hello() == "Hello, World!"
 
 
 def test_hworld_custom():
+    """Test with custom argument."""
     assert say_hello("custom") == "Hello, custom!"
