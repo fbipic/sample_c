9133abe2563cf4bc0b4f1d651e3cd5b17b322719
Fabio Piccolo <fbipic@yahoo.com>
2025-04-07 12:47:57 +0200

    Add cargo project for hworld

---
+++ code_rust/cargo_hworld/target/debug/cargo_hworld.d
@@ -0,0 +1,1 @@
+/home/fpic/prj/sample_c_github/code_rust/cargo_hworld/target/debug/cargo_hworld: /home/fpic/prj/sample_c_github/code_rust/cargo_hworld/src/main.rs
---
+++ code_rust/cargo_hworld/target/release/deps/cargo_hworld-a2a61e172db3c604.d
@@ -0,0 +1,5 @@
+/home/fpic/prj/sample_c_github/code_rust/cargo_hworld/target/release/deps/cargo_hworld-a2a61e172db3c604: src/main.rs
+
+/home/fpic/prj/sample_c_github/code_rust/cargo_hworld/target/release/deps/cargo_hworld-a2a61e172db3c604.d: src/main.rs
+
+src/main.rs:
