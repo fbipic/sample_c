94d7873f9002489d201822e8aa34ad7e4e3c5890
Fabio Piccolo <fbipic@yahoo.com>
2025-04-14 11:55:22 +0200

    Add bitwise operation example

--- code_rust/cargo_hworld/src/main.rs
+++ code_rust/cargo_hworld/src/main.rs
@@ -19,7 +19,7 @@ fn print_operation() {
 
     print!("a_f={} b_f={} ",a_f, b_f); //Print without carriage return
     // Display with 0 in front
-    println!("a_f/b_f={:08.3}", a_f/b_f);
+    println!("a_f/b_f={:08.3}", a_f as f64/b_f);
 
     //Using positinal index to print multiple time same variables
     println!("a ={0} b={1} => {0}/{1}={2:.3}", a, b, (a as f64/ b as f64));
@@ -44,10 +44,43 @@ fn print_integer() {
     //x_mut = x_mut + 1; //Perform overflow error on runtime
 }
 
+fn print_bitwise() {
+    let value= 0b1111_0101u8; //stored as unsigned int 8 bit
+    println!("value = {} -> {0:08b}", value); //display 0 and 8 bit
+
+    //Using NOT operator
+    let mut val_n = value;
+    val_n = !value;
+    println!("val_n = {:3} -> {0:08b}", val_n);
+
+    let mut val_m;
+    //Using OR operator
+    val_m = val_n & 0b1111_0111;
+    println!("val_a = {:3} -> {0:08b}", val_m);
+    println!("bit 6 = {:3}", val_m & 0b0010_0000);
+
+    //Using AND operator
+    val_m = val_m | 0b0100_0000;
+    println!("val_o = {:3} -> {0:08b}", val_m);
+
+    //Using XOR operator
+    val_m = val_m ^ 0b0101_0101;
+    println!("val_o = {:3} -> {0:08b}", val_m);
+
+    //Using shift 
+    val_m = val_m << 4;
+    println!("val_l = {:3} -> {0:08b}", val_m);
+
+    val_m = val_m >> 2;
+    println!("val_r = {:3} -> {0:08b}", val_m);
+
+}
+
 // Main program
 fn main() {
     println!("Hello, RUST world with CARGO!");
     print_integer();
     print_float();
     print_operation();
+    print_bitwise();
 }
