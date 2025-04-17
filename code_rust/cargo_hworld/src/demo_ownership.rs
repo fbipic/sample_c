db58eb1966b5b80390857cd917cfdc2efc0b31f0
Fabio Piccolo <fbipic@yahoo.com>
2025-04-17 22:02:09 +0200

    Add shadown example

--- code_rust/cargo_hworld/src/demo_ownership.rs
+++ code_rust/cargo_hworld/src/demo_ownership.rs
@@ -1,14 +1,26 @@
 pub fn run(name: &str){
     println!("==>Welcome to demo_ownership {}!!!", String::from(name));
 
-    scope();
+    scope_example();
+    shadow_example();
 }
 
 //Variable lives into curly brackets 
-fn scope() {
+fn scope_example() {
     if true {
         let planet = "Earth";
         println!("Planet is {}", planet);
     }
     //println!("Planet is {}", planet); //This cause error during build because variable is out of scope
+}
+
+fn shadow_example () {
+    let planet = "Earth";
+    {
+        println!("Planet is {}", planet);
+        let mut planet = "4";
+        println!("Planet is {}", planet);
+    }
+    //Shadow disappear after curly brackets
+    println!("Planet is {}", planet);
 }
