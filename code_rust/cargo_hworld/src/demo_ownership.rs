183a3d14b6d397f23391a1a0b3b56ce00d802554
Fabio Piccolo <fbipic@yahoo.com>
2025-04-17 22:48:47 +0200

    Add string and ownership examples

--- code_rust/cargo_hworld/src/demo_ownership.rs
+++ code_rust/cargo_hworld/src/demo_ownership.rs
@@ -3,6 +3,8 @@ pub fn run(name: &str){
 
     scope_example();
     shadow_example();
+    string_example();
+    ownership_example();
 }
 
 //Variable lives into curly brackets 
@@ -18,9 +20,50 @@ fn shadow_example () {
     let planet = "Earth";
     {
         println!("Planet is {}", planet);
-        let mut planet = "4";
+        let planet = "4";
         println!("Planet is {}", planet);
     }
     //Shadow disappear after curly brackets
     println!("Planet is {}", planet);
+}
+
+//Stack are stored in LIFO order, very quickly to access, limited in size, size is constant
+//Heap access is lower than stack, dynamically added and removed
+
+/* 
+"String" string literal, hardcoded in code, immutable, known before compile
+         string type, allocated on head, mutable
+*/
+
+fn string_example(){
+    let mut message = String::from("Earth");
+    println!("{}", message);
+    message.push_str(" is our home.");
+    println!("{}", message);
+}
+
+//Rules
+//1. Every value is owned by one and only one variable at a time
+//2. When variable goes out of scope, the value is dropped.
+
+fn ownership_example(){
+    {
+        let inner_planet = "Earth";
+        println!("inner_planet is {}", inner_planet);
+    }
+    //println!("Planet is {}", inner_planet);//Variable does not exist anymore and cause error
+
+    let outer_planet: String;
+    {
+        let mut inner_planet = String::from("in_Earth");
+        println!("inner_planet is {}", inner_planet);
+        //Here we have two pointer to same heap memory, this means in "a move"
+        //so outer_planet receives ownership of variable, because inner_planet is invalidated
+        outer_planet = inner_planet;
+        //println!("inner_planet is {}", inner_planet); //Compiler returns error because inner_ is not valid 
+        inner_planet = outer_planet.clone(); //Duplicate memory area
+        println!("inner_planet cloned is {}", inner_planet);
+    }
+    println!("outer_planet is {}", outer_planet);
+
 }
