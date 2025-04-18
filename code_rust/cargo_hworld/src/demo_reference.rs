f916b58f12f1f4b9e7ba2f83d98f01a5186e8b9d
Fabio Piccolo <fbipic@yahoo.com>
2025-04-18 13:16:17 +0200

    Add mutable borrowing example

--- code_rust/cargo_hworld/src/demo_reference.rs
+++ code_rust/cargo_hworld/src/demo_reference.rs
@@ -1,6 +1,7 @@
 pub fn run(name: &str){
     println!("==>Welcome to demo_reference {}!!!", String::from(name));
     borrow_ownership();
+    borrow_mut_ownership();
 }
 
 fn borrow_ownership(){
@@ -20,6 +21,20 @@ fn processing_fuel(propeller: String) -> (String, usize){
 
 fn processing_fuel_len(propeller: &String) -> usize{
     println!("propeller = {}", propeller);
+    let length = propeller.len();
+    length
+}
+
+fn borrow_mut_ownership(){
+    let mut rocket_fuel = String::from("RP-1");
+    let length = processing_fuel_mut(&mut rocket_fuel); 
+    println!("rocket_fuel[{}] = {}", length, rocket_fuel);
+}
+
+fn processing_fuel_mut(propeller: &mut String) -> usize {
+    //Restriction: only a mutable reference can be created
+    println!("propeller = {}", propeller);
+    propeller.push_str(" is highly flammable!");
     let length = propeller.len();
     length
 }
