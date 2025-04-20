507abe41fd2f94e000025acdbbc24e9630fad418
Fabio Piccolo <fbipic@yahoo.com>
2025-04-20 09:08:26 +0200

    Added random example using rand library

--- code_rust/cargo_hworld/src/demo_modules.rs
+++ code_rust/cargo_hworld/src/demo_modules.rs
@@ -1,9 +1,11 @@
 use std::io; //Declaration to use i/o modules
+//use rand; //Use random library available from crates.io
+use rand::{prelude::*, thread_rng}; //Load all prelude functions
 
 pub fn run(name: &str){
     println!("==>Welcome to demo_reference {}!!!", String::from(name));
     io_example();
-
+    rand_example();
 }
 
 fn io_example() {
@@ -16,4 +18,13 @@ fn io_example() {
     //let number = buffer.trim().parse::<i32>(); //to be checked 
     let number:i32 = buffer.trim().parse().unwrap();
     println!("number + 1 = {}", number+1);
+}
+
+fn rand_example()
+{
+    let number= rand::random::<f64>();
+    println!("Random number = {:.3}", number);
+
+    let number = thread_rng().gen_range(1..11);
+    println!("Generate in range = {:.3}", number);
 }
