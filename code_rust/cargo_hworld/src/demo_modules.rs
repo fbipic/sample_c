8b0c91f1d3f0c40e5278b36a76c5a70f60cd316a
Fabio Piccolo <fbipic@yahoo.com>
2025-04-20 19:02:58 +0200

    Add guessing game challenge

--- code_rust/cargo_hworld/src/demo_modules.rs
+++ code_rust/cargo_hworld/src/demo_modules.rs
@@ -6,6 +6,8 @@ pub fn run(name: &str){
     println!("==>Welcome to demo_reference {}!!!", String::from(name));
     io_example();
     rand_example();
+
+    challenge_guessing_game();
 }
 
 fn io_example() {
@@ -27,4 +29,32 @@ fn rand_example()
 
     let number = thread_rng().gen_range(1..11);
     println!("Generate in range = {:.3}", number);
+}
+
+fn challenge_guessing_game()
+{
+    let rand_num = thread_rng().gen_range(1..101);
+    //println!("Generate in range = {}", rand_num);
+    let mut win = false;
+    for i in 0..2 {
+        print!("Guess {} number:", i);
+
+        let mut buffer = String::new();
+        let err= io::stdin().read_line(&mut buffer).expect("Failed to read line");
+        println!("buffer={}\nerr={:?}", buffer, err);
+    
+        let number:i32 = buffer.trim().parse().expect("Failed to parse the guess");
+        if number == rand_num {
+            println!("You guess is right");
+            win = true;
+            break;
+        }
+        else if number > rand_num {
+            println!("You guess higher than number");
+        } else {
+            println!("You guess lower than number");
+        }
+    }
+    if win { println!("You win!")} else { println!("You lost")}
+
 }
