312b49a1111d9726b6d155d7aec5a7ac3ca9b17f
Fabio Piccolo <fbipic@yahoo.com>
2025-04-21 09:02:02 +0200

    Add write file example and add output into .gitignore

--- code_rust/cargo_hworld/src/demo_io.rs
+++ code_rust/cargo_hworld/src/demo_io.rs
@@ -1,11 +1,14 @@
 use std::env; //Include to use arguments passed via command line
 use std::fs; //Include to use files
+//use std::io::Write; //Include to allow to write to file
+use std::io::prelude::*; //Include to allow to read, write to file
 
 pub fn run(name: &str){
     println!("==>Welcome to demo_io {}!!!", String::from(name));
 
     cmd_line_args_example();
     read_file_example();
+    write_file_example();
 }
 
 fn cmd_line_args_example() {
@@ -30,6 +33,21 @@ fn read_file_example() {
         println!("line {} = {}", index, line);
     }
 
+    //Read file bytes
     let contents = fs::read("planets.txt").unwrap();
     println!("contents=<{:?}>", contents);   
+}
+
+fn write_file_example() {
+    let mut speech = String::new();
+    speech.push_str("We choose to go to the Moon in this decade\n");
+    speech.push_str("and do the other things,\n");
+    speech.push_str("not because they are easy,\n");
+    speech.push_str("but because they are hard.");
+
+    //This function writes all contents in one time and overwrite existing file
+    fs::write("speech.txt", speech);
+
+    let mut file = fs::OpenOptions::new().append(true).open("planets.txt").unwrap();
+    file.write(b"\nPluto");
 }
