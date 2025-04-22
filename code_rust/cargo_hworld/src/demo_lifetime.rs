1903a2f86bb549f32038e8641e04221781d08c34
Fabio Piccolo <fbipic@yahoo.com>
2025-04-22 16:05:51 +0200

    Add annotation lifetime example

--- code_rust/cargo_hworld/src/demo_lifetime.rs
+++ code_rust/cargo_hworld/src/demo_lifetime.rs
@@ -2,6 +2,7 @@ pub fn run(name: &str){
     println!("==>Welcome to demo_lifetime {}!!!", String::from(name));
 
     borrow_checker_example();
+    lifetime_annotation_syntax_example();
 }
 
 fn borrow_checker_example () {
@@ -16,4 +17,29 @@ fn borrow_checker_example () {
     //This cause an error, rp1 is no longer available
     //Only possible solution is to move rp1 declaration before curly brackets
     println!("propellant is {}", propellant); 
+}
+
+//fn best_fuel(x: &str, y: &str) -> &str { //Error because expected named lifetime parameter
+//This syntax tell how manage lifetime by compiler
+fn best_fuel<'a>(x: &'a str, y: &'a str) -> &'a str  {  
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn lifetime_annotation_syntax_example() {
+    let result;
+    let propellant1 = String::from("RP-1");
+    let propellant2 = String::from("LNG");
+    result = best_fuel(&propellant1, &propellant2);
+    println!("result is {}", result);
+
+    let result2;
+    {
+        let propellant3 = String::from("LNG");
+        result2 = best_fuel(&propellant1, &propellant3); //THis fails
+    }
+    println!("result2 is {}", result2);
 }
