acc75c4c2871ff937e79e8d9519ad7dffaa0a80b
Fabio Piccolo <fbipic@yahoo.com>
2025-04-22 07:56:56 +0200

    Add box data type example

--- code_rust/cargo_hworld/src/demo_gen_type.rs
+++ code_rust/cargo_hworld/src/demo_gen_type.rs
@@ -1,8 +1,11 @@
+use std::mem; //Added in box example
+
 pub fn run(name: &str){
     println!("==>Welcome to demo_gen_type {}!!!", String::from(name));
     gen_struct_example();
     gen_type_methods();
     gen_function_example();
+    box_type_example();
 }
 
 #[derive(Debug)]    
@@ -60,4 +63,30 @@ fn get_biggest<T: PartialOrd>(a:T, b:T) -> T {
 
 fn gen_function_example(){
     println!("biggest is {}", get_biggest(3.1, 5.1));
+}
+
+struct Shuttle {
+    name: String,
+    crew_size: u8,
+    propellant: f64
+}
+
+fn box_type_example() {
+    let vehicle = Shuttle {
+        name: String::from("Atlantis"),
+        crew_size: 7,
+        propellant: 835958.0
+    };
+    println!("vehicle size on stack: {} bytes", mem::size_of_val(&vehicle));
+
+    //After this point vehicle lose ownership
+    let boxed_vehicle: Box<Shuttle> = Box::new(vehicle);
+    //Here we get occupation of a pointer on stack
+    println!("boxed_vehicle size on stack: {} bytes", mem::size_of_val(&boxed_vehicle));
+    //Here we get occupation of structure on heap
+    println!("boxed_vehicle size on stack: {} bytes", mem::size_of_val(&*boxed_vehicle));
+
+    //Return ownership back to unboxed_vehicle
+    let unboxed_vehicle: Shuttle = *boxed_vehicle;
+    println!("unboxed_vehicle size on stack: {} bytes", mem::size_of_val(&unboxed_vehicle));
 }
