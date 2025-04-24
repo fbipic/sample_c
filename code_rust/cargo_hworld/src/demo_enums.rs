5ece5c9dee765ec8c94840c1b24f1b30d3b5172b
Fabio Piccolo <fbipic@yahoo.com>
2025-04-24 16:20:49 +0200

    Add enum definition example

--- code_rust/cargo_hworld/src/demo_enums.rs
+++ code_rust/cargo_hworld/src/demo_enums.rs
@@ -1,4 +1,16 @@
 pub fn run(name: &str){
     println!("==>Welcome to demo_enums {}!!!", String::from(name));
+    define_enum_example();
+}
 
+#[derive(Debug)]
+enum Shape {
+    Circle(f64), //Can specify parameter with unknown name for every element of enum
+    Triangle(f64, f64, f64), 
+    Rectangle(f64, f64)
+}
+
+fn define_enum_example() {
+    let my_shape = Shape::Rectangle(1.2, 3.4);
+    println!("my_shape = {:?}", my_shape);
 }
