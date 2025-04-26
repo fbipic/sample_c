22eaad97af7be2139a2bb019f75bc1c84e3b7a6c
Fabio Piccolo <fbipic@yahoo.com>
2025-04-26 16:18:08 +0200

    Add match error example

--- code_rust/cargo_hworld/src/demo_errors_handling.rs
+++ code_rust/cargo_hworld/src/demo_errors_handling.rs
@@ -1,10 +1,12 @@
 use std::fs;
+use std::io;
 
 pub fn run(name: &str){
     println!("==>Welcome to demo_errors_handling {}!!!", String::from(name));
 
     unrecoverable_error_example();
     recoverable_error_example();
+    match_result_example();
 }
 
 //Recoverable errors -> enum Result<T,E>
@@ -26,5 +28,23 @@ fn recoverable_error_example(){
     println!("Content is {:?}", content); //output of enum Result is displayed
 
     //We cannot extract value from enum Result directly so we can use .unwrap(), but in case of failure it does not work
+    //let content = fs::read_to_string("the_ultimate_question_result.txt").unwrap(;
     //.expect() can be used to trigger a specific message in exception
+    //let content = fs::read_to_string("the_ultimate_question_result.txt").expect("Nobody knows it");
+}
+
+fn match_result_example() {
+    let result = fs::read_to_string("the_ultimate_question_result.txt");
+
+    let contents = match result {
+        Ok(message) => message,
+        //Err(error) => String::from("Nobody knows it!")
+        Err(error) => match error.kind() {
+            io::ErrorKind::NotFound => String::from("File not found!"),
+            io::ErrorKind::PermissionDenied => String::from("Permission denied!"),
+            _ => panic!("There is another type of error")
+        }
+    };
+
+    println!("Contents is {:?}", contents); //output of enum Result is displayed
 }
