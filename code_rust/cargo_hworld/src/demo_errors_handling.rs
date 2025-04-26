67f114f009f216f99f7f6f4c299e792c83f88f5a
Fabio Piccolo <fbipic@yahoo.com>
2025-04-26 16:05:39 +0200

    Add demo for recoverable example

--- code_rust/cargo_hworld/src/demo_errors_handling.rs
+++ code_rust/cargo_hworld/src/demo_errors_handling.rs
@@ -1,10 +1,13 @@
+use std::fs;
+
 pub fn run(name: &str){
     println!("==>Welcome to demo_errors_handling {}!!!", String::from(name));
 
     unrecoverable_error_example();
+    recoverable_error_example();
 }
 
-//Recoverable errors -> Result<T,E>
+//Recoverable errors -> enum Result<T,E>
 //Unrecoverable errors -> panic! macro
 fn unrecoverable_error_example() {
     //panic!("Houston, we have a problem!"); //This cause program to exit with 101 error
@@ -16,4 +19,12 @@ fn unrecoverable_error_example() {
         let x = 1 / count; //This will fails => divide by zero
     }
     //Use RUST_BACKTRACE=1 to get more information about error.
+}
+
+fn recoverable_error_example(){
+    let content = fs::read_to_string("the_ultimate_question_result.txt");
+    println!("Content is {:?}", content); //output of enum Result is displayed
+
+    //We cannot extract value from enum Result directly so we can use .unwrap(), but in case of failure it does not work
+    //.expect() can be used to trigger a specific message in exception
 }
