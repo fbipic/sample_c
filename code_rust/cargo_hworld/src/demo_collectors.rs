383c87051ab793ce1f462adcd1db1d4267f38464
Fabio Piccolo <fbipic@yahoo.com>
2025-04-28 10:33:30 +0200

    Add hash map example

--- code_rust/cargo_hworld/src/demo_collectors.rs
+++ code_rust/cargo_hworld/src/demo_collectors.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap; //Required to be added
+
 pub fn run(name: &str){
     println!("==>Welcome to demo_collectors {}!!!", String::from(name));
 
     vector_example();
+    hash_map_example();
 }
 
 fn vector_example(){
@@ -23,4 +26,27 @@ fn vector_example(){
     //Macro to define vector
     let countdown = vec![5,4,3,2,1,0];
     println!("countdown = {:?}", countdown);
+}
+
+fn hash_map_example() {
+    let mut missions_flown = HashMap::new(); // missions flown as of 1 Jan 2021
+    missions_flown.insert("Hadfield", 3); // Chris Hadfield
+    missions_flown.insert("Hurley", 3); // Doug Hurley
+    missions_flown.insert("Barron", 0); // Kayla Barron
+
+    //Manage hash map
+    missions_flown.insert("Barron", 1); //Override existing key-value pair
+    //Insert new entry if does not exist
+    missions_flown.entry("Barron").or_insert(2); //Key already exist, so this does not change value
+    missions_flown.entry("Stone").or_insert(2); //New value pair will be inserted
+    //Modify value based on its existing value
+    let kayla = missions_flown.entry("Barron").or_insert(0); //This return reference
+    *kayla += 1;
+    println!("missions_flown is {:?}", missions_flown);
+
+    let barron_mission = missions_flown.get("Barron");
+    println!("Barron missions are {:?}", barron_mission);
+
+    
+
 }
