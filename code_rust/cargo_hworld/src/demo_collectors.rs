9dc09ccc418c5b78f656504e6de10d5c6532f191
Fabio Piccolo <fbipic@yahoo.com>
2025-04-28 10:14:52 +0200

    Add vector example

--- code_rust/cargo_hworld/src/demo_collectors.rs
+++ code_rust/cargo_hworld/src/demo_collectors.rs
@@ -1,3 +1,26 @@
 pub fn run(name: &str){
     println!("==>Welcome to demo_collectors {}!!!", String::from(name));
+
+    vector_example();
+}
+
+fn vector_example(){
+    let mut astronauts: Vec<String> = Vec::new();
+    astronauts.push(String::from("Shepard")); // Alan Shepard
+    astronauts.push(String::from("Grissom")); // Gus Grissom
+    astronauts.push(String::from("Glenn")); // John Glenn
+    println!("astronauts is {:?}", astronauts);
+
+    let last = astronauts.pop();
+    println!("last is {:?}", last);
+
+    //We need to use borrow operator because we cannot pass string ownership to variable
+    //let third = &astronauts[2]; //Adding code above remove last element use get instead
+    //println!("third astronaut is {}", third);
+    let third = astronauts.get(2); //return enum option
+    println!("third astronaut is {:?}", third);
+
+    //Macro to define vector
+    let countdown = vec![5,4,3,2,1,0];
+    println!("countdown = {:?}", countdown);
 }
